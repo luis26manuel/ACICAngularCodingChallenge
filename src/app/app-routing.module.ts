8159449bb9fc5a7077113b08fa4296e6594a7ac7
lmfajard <lmfajard@ncsu.edu>
2023-08-09 20:24:48 -0400

    Removed commented out code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LineOfBusinessComponent } from './linesOfBusiness/linesOfBusiness.component';
 import { LineOfBusinessDetailComponent } from './lineOfBusiness-detail/lineOfBusiness-detail.component';
-//import { LineOfBusinessAmountsComponent } from './lineOfBusiness-amounts/lineOfBusiness-amounts.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  //{ path: 'amounts', component: LineOfBusinessAmountsComponent },
   { path: 'detail/:id', component: LineOfBusinessDetailComponent },
   { path: 'linesOfBusiness', component: LineOfBusinessComponent }
 ];
